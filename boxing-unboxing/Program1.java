6f4e292ff985bce13f05f97249a4b8bc03cca432
anuradha-mudhale <anuradhamudhale314@gmail.com>
2025-07-25 19:38:19 +0530

    Add files via upload

---
+++ boxing-unboxing/Program1.java
@@ -0,0 +1,14 @@
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		//Converting primitive to wrapper --> Boxing 
+		int a = 10;
+		Integer b = new Integer(a); // Boxing 
+		
+		//Converting wrapper to primitive -> unboxing 
+		int c = b.intValue(); // Unboxing  
+
+	}
+
+}
